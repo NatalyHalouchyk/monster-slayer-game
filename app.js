4dc0c73ea975ad474520dbb089650cdab93a1395
NatalyHalouchyk <natallia.halouchyk@gmail.com>
2022-04-05 18:00:29 +0200

    Add functionality to 'surrender' button, add 'Game over' screen

--- app.js
+++ app.js
@@ -8,18 +8,43 @@ const app = Vue.createApp({
     return {
       monsterHealth: 100,
       playerHealth: 100,
-      currentRound: 0
+      currentRound: 0,
+      winner: null
     }
   },
   computed: {
     monsterBarStyles() {
-      return {width: this.monsterHealth + '%'};
+      if(this.monsterHealth < 0){
+        return {width: '0%'};
+      } else {
+        return {width: this.monsterHealth + '%'};
+      }
     },
     playerBarStyles() {
-      return {width: this.playerHealth + '%'};
+      if (this.playerHealth < 0){
+        return {width : '0%'};
+      } else {
+        return {width: this.playerHealth + '%'};
+      }
     },
     mayUseSpecialAttack(){
       return this.currentRound % 3 !== 0;
+    }
+  },
+  watch: {
+    playerHealth(value){
+      if (value <=0 && this.monsterHealth<=0){
+        this.winner = "draw";
+      } else if (value <=0){
+       this.winner = "monster";
+      }
+    },
+    monsterHealth (value){
+      if (value <= 0 && this.playerHealth <= 0){
+        this.winner = "draw";
+      } else if (value <= 0){
+        this.winner = "player";
+      }
     }
   },
   methods: {
@@ -40,7 +65,7 @@ const app = Vue.createApp({
       this.attackPlayer();
     },
     healPlayer(){
-      currentRound++;
+      this.currentRound++;
       const healValue = getRandomValue(8, 20);
       if (this.playerHealth + healValue > 100){
         this.playerHealth = 100;
@@ -48,6 +73,15 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       this.attackPlayer();
+    },
+    startNewGame(){
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
+    surrender(){
+      this.winner = "monster";
     }
   }
 });
